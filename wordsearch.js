a3c10602712ccdd1083940466757cafca5cb59b9
Miranda Zheng <zhengshuang2011@gmail.com>
2021-12-02 21:50:41 +0000

    Create a function to check if the word is included in the matrix or not

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require("../../d2/matrix_transposition");
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (word === "") {
+    return false;
+  }
+  word = word.toUpperCase();
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const VerticalJoin = transpose(letters).map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of VerticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
